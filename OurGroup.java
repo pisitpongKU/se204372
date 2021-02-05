af40e98f479455b8df1421149393fb2a4a2507c3
pisitpongKU <pisitpong.ph@ku.th>
2021-02-05 12:41:08 +0700

    My first group changes to OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -6,7 +6,7 @@ public class OurGroup {
     public OurGroup()
     {
         groupMembers = new Vector<String>();
-        groupMembers.add("YOUR NAME"); //PUT YOUR NAME HERE
+        groupMembers.add("Pisitpong Phochai"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
